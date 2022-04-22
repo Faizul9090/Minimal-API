7c8678cc2924550664f4d1ba387cf90eef5fb4c3
Faizul9090 <15203120@iubat.edu>
2022-04-23 05:11:42 +0600

    Added db context for work with entityFramework

--- MiniDemo/Model/Employee.cs
+++ MiniDemo/Model/Employee.cs
@@ -7,39 +7,40 @@
         public string Sitizenship { get; set; }
     }
 
-    public class EmployeeCollection
-    {
-        public List<Employee> Employees { get; set; }
+    /* Hard code */
+    //public class EmployeeCollection
+    //{
+    //    public List<Employee> Employees { get; set; }
 
-        public List<Employee> GetEmployees()
-        {
-            return new List<Employee>()
-            {
-                 new Employee()
-                {
-                    EmployeeId = "1",
-                    Name = "Faizul",
-                    Sitizenship = "Bangladesh"
-                },
-                new Employee()
-                {
-                    EmployeeId = "2",
-                    Name = "Tareque",
-                    Sitizenship = "Bangladesh"
-                },
-                new Employee()
-                {
-                    EmployeeId = "3",
-                    Name = "Joy",
-                    Sitizenship = "India"
-                },
-                new Employee()
-                {
-                    EmployeeId = "4",
-                    Name = "Feroz",
-                    Sitizenship = "USA"
-                }
-            };
-        }
-    }
+    //    public List<Employee> GetEmployees()
+    //    {
+    //        return new List<Employee>()
+    //        {
+    //             new Employee()
+    //            {
+    //                EmployeeId = "1",
+    //                Name = "Faizul",
+    //                Sitizenship = "Bangladesh"
+    //            },
+    //            new Employee()
+    //            {
+    //                EmployeeId = "2",
+    //                Name = "Tareque",
+    //                Sitizenship = "Bangladesh"
+    //            },
+    //            new Employee()
+    //            {
+    //                EmployeeId = "3",
+    //                Name = "Joy",
+    //                Sitizenship = "India"
+    //            },
+    //            new Employee()
+    //            {
+    //                EmployeeId = "4",
+    //                Name = "Feroz",
+    //                Sitizenship = "USA"
+    //            }
+    //        };
+    //    }
+    //}
 }
